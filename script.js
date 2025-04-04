a511b70562c174abc0cb4b64248b161d1058f1ba
ThamsanqaEmmanuel <ethamsanqa@gmail.com>
2025-04-04 07:43:32 +0200

    finished

---
+++ script.js
@@ -0,0 +1,7 @@
+function viewCV() {
+    const confirmed = confirm("You are about to view Thamsanqa's CV");
+    if (confirmed) {
+      // Replace 'thamsanqa-cv.pdf' with your actual CV file path or link
+      window.open('ThamsanqaCV.pdf', '_blank');
+    }
+  }
